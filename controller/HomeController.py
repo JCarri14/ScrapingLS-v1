57962ffedcdb5d3dc9e4fe476d49e96536bc19a9
Jcarri14 <j.carrion@students.salle.url.edu>
2020-03-27 14:05:06 +0100

    Update HomeController.py

--- controller/HomeController.py
+++ controller/HomeController.py
@@ -1,24 +1,24 @@
-from ddbb.dbManager import Database
+from ddbb.DBManager import DBManager
 from scraping import SourceManager
 from view.HomeView import HomeView
-#from controller.MainController import Controller
 
 
 class HomeController:
-    def __init__(self, master_view, master_controller, db_manager):
+    def __init__(self, master_view, master_controller):
         self.view = HomeView(master_view)
         self.items = []
         self.parentView = master_view
         self.parentController = master_controller
-        self.db_manager = db_manager
+        self.db_manager = DBManager()
+        print()
 
 
     def show_view(self):
         self.view = HomeView(self.parentView)
         self.view.init_frame()
         self.view.btnSearch.config(command=self.on_search_request)
-        if len(self.db_manager.sources) == 0:
-            self.db_manager.requestSources()
+        if len(self.db_manager.get_sources()) == 0:
+            self.db_manager.get_items("sources")
         self.view.update_src_items(self.db_manager.sources)
         self.get_news_items()
 
@@ -26,7 +26,10 @@ class HomeController:
         self.view.destroy_view()
 
     def get_news_items(self):
-        items = SourceManager.get_data(self.db_manager.sources[0])
+        items = []
+        if len(self.db_manager.sources) > 0:
+            print()
+            #items = SourceManager.get_data(self.db_manager.instance.sources[0])
         self.view.update_news_items(items)
 
     def on_search_request(self):
