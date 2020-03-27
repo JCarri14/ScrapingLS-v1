bce5c00a2de5a27c9ce9e657525ea8d8f31fec6c
Jcarri14 <j.carrion@students.salle.url.edu>
2020-03-27 10:02:06 +0100

    Create Constants.py

---
+++ utils/Constants.py
@@ -0,0 +1,41 @@
+# CONSTANTS -------------------------------------------------------------------------
+
+# SCREEN ATTRIBUTES -----------------------------------------------------------------
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+SCREEN_SIZE = str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT)
+
+# APP ATTRIBUTES --------------------------------------------------------------------
+APP_TITLE = "ScrapingLS"
+
+# COLOR VALUES ----------------------------------------------------------------------
+COLOR_PRIMARY = "#304ffe"
+COLOR_PRIMARY_DARK = "#1a237e"
+COLOR_SECONDARY = ""
+COLOR_SECONDARY_DARK = ""
+COLOR_ERROR = "#d50000"
+COLOR_DARK = "#212121"
+COLOR_BLACK = "#000000"
+COLOR_WHITE = "#FFFFFF"
+
+# FILTER LABELS ---------------------------------------------------------------------
+ITEM_CONTAINER_REF = "Item Container Reference" # Meant to be the 1st filter to apply
+FILTERS_LABEL = "Filters"
+FILTER_LABEL = "Filter"
+FILTER_RESULT = "Expected Result"
+RESULT_VALUES = ["item-container", "title", "thumbnail", "description", "created_at", "times_shared", "comments"]
+
+# STRING VALUES ---------------------------------------------------------------------
+NAME_LABEL = "Name"
+URL_LABEL = "Url"
+VALUE_LABEL = "Value"
+HOME_LABEL = "Home"
+WE_LABEL = "We"
+STATS_LABEL = "Stats"
+SEARCH_LABEL = "Search"
+SRC_LABEL = "Sources"
+ADD_LABEL = "Add"
+ADD_SRC_LABEL = "Add new Source"
+UPDATE_LABEL = "Update"
+DELETE_LABEL = "Delete"
+CANCEL_LABEL = "Cancel"
