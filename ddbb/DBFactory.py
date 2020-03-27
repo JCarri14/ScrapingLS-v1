051d40c4886cf0e392559dd4e4c270cc41268fd7
Jcarri14 <j.carrion@students.salle.url.edu>
2020-03-27 14:04:41 +0100

    Create DBFactory.py

---
+++ ddbb/DBFactory.py
@@ -0,0 +1,8 @@
+class DBFactory:
+    def close(self): pass
+    def set_item_location(self, location): pass
+    def insert_item(self, item): pass
+    def update_item(self, item): pass
+    def delete_item(self, item): pass
+    def get_items(self): pass
+    def get_item_by_name(self, name): pass
