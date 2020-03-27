5cc3ba0b82a357e939f1b706412306155c35ae62
Jcarri14 <j.carrion@students.salle.url.edu>
2020-03-27 14:05:04 +0100

    Update MainController.py

--- controller/MainController.py
+++ controller/MainController.py
@@ -1,22 +1,29 @@
-from ddbb.dbManager import Database
+from ddbb.DBManager import DBManager
 from view.MainView import MainView
 from controller.HomeController import HomeController
 from controller.SourcesController import SourceController
 from utils.Constants import *
 
+
 class Controller:
     def __init__(self, root):
-        self.db_manager = Database()
+        self.db_manager = DBManager()
+        self.db_manager.setFactorySource("Mongo")
         self.mainView = MainView(root)
         self.mainView.init_view()
-        self.homeController = HomeController(self.mainView, self, self.db_manager)
-        self.srcController = SourceController(self.mainView, self, self.db_manager)
+        self.config_upper_panel_btns()
+        self.init_controllers()
+        self.homeController.show_view()
+
+    def config_upper_panel_btns(self):
         self.mainView.btnHome.config(command=self.on_home_request)
         self.mainView.btnSrc.config(command=self.on_sources_request)
         self.mainView.btnStats.config(command=self.on_stats_request)
-        self.mainFrame = root
+
+    def init_controllers(self):
+        self.homeController = HomeController(self.mainView, self)
+        self.srcController = SourceController(self.mainView, self)
         self.currentController = self.homeController
-        self.homeController.show_view()
 
     def on_home_request(self):
         if self.currentController != self.homeController:
